d94e2b7acae2b58ca84a56aa3262f5952a741991
Andrzej Ładomirski <andrzejladomirski@gmail.com>
2022-12-18 15:48:52 +0100

    ?

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [&](Human& k) { k.birthday(); });
+
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& k) {
+        return k.isMonster() ? 'n' : 'y';
+    });
 
     return ret_v;
 }
